f556672d9bdd5abe33e9197527524c18c21dd512
Parag Gupta <sevenaces@gmail.com>
2013-07-02 23:21:44 +0530

    INIT

--- slcc/Assets/JS/main.js
+++ slcc/Assets/JS/main.js
@@ -1,3 +1,5 @@
+alert(1);
+
 var data = [1,2,4,5,4];
 
 var list = d3.select("body").append('ul');
@@ -6,4 +8,4 @@ var items = list.selectAll('li').data(data);
 
 items.enter()
 	.append('li')
-	.text("test");
+	.text(function(d){return d;});
