36fc570966712f889616be5d0ace28172f8fe18c
Parag Gupta <sevenaces@gmail.com>
2013-07-03 22:50:04 +0530

    geojson

--- hellod3/js/basic.js
+++ hellod3/js/basic.js
@@ -1,5 +1,17 @@
 // Basic JS for the website
 
-$('.nav li a').click(function(){
+$(window).ready(function(){
+	if(document.location.toString().indexOf('#') > 0)
+	{
+		var pid = document.location.toString().substring(document.location.toString().indexOf('#') + 1);
+		$('#content').load('./' + pid + '.html');
+		$('#' + pid).parent().attr('class', 'active');
+	}
+});
+
+$('.nav li a').click(function(e){
+	//e.preventDefault();
 	$('#content').load('./' + this.id + '.html');
+	$('.nav li').removeClass('active');
+	$('#' + this.id).parent().attr('class', 'active');
 });
