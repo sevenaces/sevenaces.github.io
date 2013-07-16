c6198ccce5166aac1a58b413bc41266c8dd87b91
Parag Gupta <sevenaces@gmail.com>
2013-07-16 22:38:37 +0530

    AmericanHealth

---
+++ americanhealth/js/basic.js
@@ -0,0 +1,10 @@
+// Basic JS for the website
+
+// $(window).ready(function(){
+// 	if(document.location.toString().indexOf('#') > 0)
+// 	{
+// 		var pid = document.location.toString().substring(document.location.toString().indexOf('#') + 1);
+// 		$('#content').load('./' + pid + '.html');
+// 		$('#' + pid).parent().attr('class', 'active');
+// 	}
+// });
